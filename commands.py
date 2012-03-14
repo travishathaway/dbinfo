fa0fc7d013bfd890cca29f053547c0574f7cd24c
Travis Hathaway <travis.j.hathaway@gmail.com>
2012-03-14 16:19:41 -0700

    add another command line option for disabling databases

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import argparse,random,string
 from queries import (Queries,getcon)
 
+from local_settings import DB_BACKUP_DIR
 
 class DisableUser(argparse.Action):
     '''
@@ -31,9 +32,9 @@ class DisableUser(argparse.Action):
             user_data = self.cursor.fetchall()
             if user_data:
                 for row in user_data:
-                    answer = raw_input("Are you sure you want to disable " +user+"@"+row.get('host')+" account(y/n)? ")
-                    password = self.gen_password()
-                    if answer == 'y':
+                    answer = raw_input("Are you sure you want to disable " +user+"@"+row.get('host')+" account(Y/n)? ")
+                    if answer in ['y','']:
+                        password = self.gen_password()
                         self.cursor.execute("SET PASSWORD FOR '"+row.get('user')+"'@'"+row.get('host')+ "' = PASSWORD('"+password+"')")
                         print "User "+user+"@"+row.get('host')+" password has been reset"
             else:
@@ -54,6 +55,57 @@ class DisableUser(argparse.Action):
                 break
         return password
 
+class DisableDb(argparse.Action):
+
+    def __init__(self,option_strings,dest,
+                    nargs=None,const=None,
+                    default=None,type=None,
+                    choices=None,required=False,
+                    help=None,metavar=None):
+        argparse.Action.__init__(self,
+                                option_strings=option_strings,
+                                dest=dest,nargs=nargs,const=const,
+                                default=default,type=type,choices=choices,
+                                required=required,help=help,metavar=metavar)
+        db = getcon()
+        self.cursor = db.cursor(cursorclass=Queries)
+
+    def __call__(self,parser,namespace,values,option_string=None):
+        self.drop_db(values)
+
+    def backup_db(self,database,backup_dir):
+        '''
+        Function that backs up a mysql database (wrapper around `mysqldump`)
+        '''
+        args = ['mysqldump',database,'--user='+traviSQL.dbuser,'--host='+self.dbhost,'-p'+traviSQL.dbpass]
+        backup_sql = open(backup_dir+'/'+database+'.sql', 'w')
+        return subprocess.call(args,stdout=backup_sql)
+
+    def drop_db(self,databases):
+        '''
+        Function that drops the given databases in a MySQL database
+        '''
+
+        for database in databases:
+            self.cursor.execute('SHOW DATABASES LIKE %s', database)
+            if self.cursor.fetchall():
+                answer = raw_input("(a backup will be created in "+DB_BACKUP_DIR+")\n \
+                                    Are you sure you want to drop "+database+"(Y/n)?")
+                if answer in ['y','']:
+                    print "Creating backup at "+self.backup_dir+"/"+database+".sql"
+                    err_code = self.backup_db(database,DB_BACKUP_DIR)
+                    if err_code == 0:
+                        self.cursor.execute("DROP DATABASE `"+database+"`")
+                        print "Database "+database+" has been deleted"
+                    else:
+                        print "Problem creating backup for "+database+". Abandoning..."
+            else: 
+                print "database "+database+" not found"
+
+parser = argparse.ArgumentParser(description='Disable MySQL user accounts by resetting the password.')
+parser.add_argument('--user', '-u', nargs="+",type=str, help="One or more MySQL user accounts to disable.")
+parser.add_argument('--database', '-d', nargs="+",type=str, help="One or more MySQL databases to drop. These databases will backed up to the ARC back dir.")
+
 
 def add_args():
 
@@ -64,7 +116,7 @@ def add_args():
     parser_disable_user.add_argument('users',nargs="+",action=DisableUser)
 
     parser_disable_db = subparsers.add_parser('disable_db', help="One or more databases to back up and delete.")
-    parser_disable_db.add_argument('databases', nargs="+",
+    parser_disable_db.add_argument('databases', nargs="+",action=DisableDb,
                                     help="One or more MySQL databases to drop and backup")
 
     parser_reports = subparsers.add_parser('reports', help="Generate reports about MySQL server")
